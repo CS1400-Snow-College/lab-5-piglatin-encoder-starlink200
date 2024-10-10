9868399cb44ca4e9163b7eef1501e48e2e718e50
Caleb Roskelley <caleb.roskelley@students.snow.edu>
2024-10-09 22:44:14 -0600

    working on rejecting phrases with numbers

--- Program.cs
+++ Program.cs
@@ -4,69 +4,79 @@ Console.WriteLine("Hello! This program will allow the user to give a phrase, tra
 Console.WriteLine("Please give a phrase.");
 string userPhrase = Console.ReadLine();
 string[] phraseArray = userPhrase.Split();
+bool numberFound = false;
 
 //for each statement to create the phrase in pig latin to later encrypt phrase
 string pigLatinPhrase = "";
-foreach(string word in phraseArray)
+for(int i = 0; i < userPhrase.Length - 1; i++)
 {
-    switch(word[0])
-    {
-        case 'a':
-        case 'e':
-        case 'i':
-        case 'o':
-        case 'u':
-        case 'y':
-            pigLatinPhrase += word + "way ";
-            break;
-        default:
-            int count = 0;
-            bool leaveForEach = false;
-            foreach(char letter in word)
-            {
-                switch(letter)
-                {
-                    case 'a':
-                    case 'e':
-                    case 'i':
-                    case 'o':
-                    case 'u':
-                    case 'y':
-                        pigLatinPhrase += word.Substring(count, word.Length - count) + word.Substring(0, count) + "ay ";
-                        leaveForEach = true;
-                        break;
-                    default:
-                        break;
-                }
-                //once the word is changed we want the foreach loop to stop
-                if(leaveForEach)
-                {
-                    break;
-                }
-                count++;
-            }
-            break;
-    }
+    string letter = userPhrase.Substring(i,i+1);
+    numberFound = int.TryParse(letter, out int nums);
 }
-Console.WriteLine(pigLatinPhrase);
+if(!numberFound)
+{
 
-//using nested for each statement to shift each letter by a certain number
-string[] pigLatinArray = pigLatinPhrase.Split();
-string encryptedPhrase = "";
-//random number for the encryption off set
-int randOffSet = rand.Next(1, 25);
-foreach(string word in pigLatinArray)
-{
-    foreach(char letter in word)
+    foreach(string word in phraseArray)
     {
-        if((int)letter + randOffSet < 123){
-            encryptedPhrase += (char)((int)letter + randOffSet);
-        }
-        else
+        switch(word[0])
         {
-            encryptedPhrase += (char)((int)letter + randOffSet - 26);
+            case 'a':
+            case 'e':
+            case 'i':
+            case 'o':
+            case 'u':
+            case 'y':
+                pigLatinPhrase += word + "way ";
+                break;
+            default:
+                int count = 0;
+                bool leaveForEach = false;
+                foreach(char letter in word)
+                {
+                    switch(letter)
+                    {
+                        case 'a':
+                        case 'e':
+                        case 'i':
+                        case 'o':
+                        case 'u':
+                        case 'y':
+                            pigLatinPhrase += word.Substring(count, word.Length - count) + word.Substring(0, count) + "ay ";
+                            leaveForEach = true;
+                            break;
+                        default:
+                            break;
+                    }
+                    //once the word is changed we want the foreach loop to stop
+                    if(leaveForEach)
+                    {
+                        break;
+                    }
+                    count++;
+                }
+                break;
         }
     }
-    encryptedPhrase += " ";
+    Console.WriteLine(pigLatinPhrase);
+
+    //using nested for each statement to shift each letter by a certain number
+    string[] pigLatinArray = pigLatinPhrase.Split();
+    string encryptedPhrase = "";
+    //random number for the encryption off set
+    int randOffSet = rand.Next(1, 25);
+    foreach(string word in pigLatinArray)
+    {
+        foreach(char letter in word)
+        {
+            if((int)letter + randOffSet < 123){
+                encryptedPhrase += (char)((int)letter + randOffSet);
+            }
+            else
+            {
+                encryptedPhrase += (char)((int)letter + randOffSet - 26);
+            }
+        }
+        encryptedPhrase += " ";
+    }
+    Console.WriteLine(encryptedPhrase);
 }
-Console.WriteLine(encryptedPhrase);
--- obj/Debug/net8.0/lab-5-piglatin-encoder-starlink200.AssemblyInfo.cs
+++ obj/Debug/net8.0/lab-5-piglatin-encoder-starlink200.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("lab-5-piglatin-encoder-starlink200")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e8a7e3ae516b19f60fbfd863981f24d608100fbb")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0c262aa4c899210392c13a3bcd2f3f1e8906f63c")]
 [assembly: System.Reflection.AssemblyProductAttribute("lab-5-piglatin-encoder-starlink200")]
 [assembly: System.Reflection.AssemblyTitleAttribute("lab-5-piglatin-encoder-starlink200")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
